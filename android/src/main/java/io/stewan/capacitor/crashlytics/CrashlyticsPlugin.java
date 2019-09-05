c47b78fe59f72dad311ef9df7ca86bc378902b57
Stewan <oi@stewan.io>
2019-09-05 12:06:42 -0300

    feat(android): wip

--- android/src/main/java/io/stewan/capacitor/crashlytics/CrashlyticsPlugin.java
+++ android/src/main/java/io/stewan/capacitor/crashlytics/CrashlyticsPlugin.java
@@ -1,20 +1,62 @@
 package io.stewan.capacitor.crashlytics;
 
-import com.getcapacitor.JSObject;
+import android.Manifest;
+
 import com.getcapacitor.NativePlugin;
 import com.getcapacitor.Plugin;
 import com.getcapacitor.PluginCall;
 import com.getcapacitor.PluginMethod;
 
-@NativePlugin()
+import com.crashlytics.android.Crashlytics;
+
+
+import io.fabric.sdk.android.Fabric;
+
+import static com.crashlytics.android.Crashlytics.*;
+
+
+@NativePlugin(
+        permissions = {
+                Manifest.permission.ACCESS_NETWORK_STATE,
+                Manifest.permission.INTERNET,
+                Manifest.permission.WAKE_LOCK
+        }
+)
 public class CrashlyticsPlugin extends Plugin {
+
+    public void load() {
+        Fabric.with(getContext(), new Crashlytics());
+    }
 
     @PluginMethod()
     public void crash(PluginCall call) {
-        String value = call.getString("value");
+        // throw new RuntimeException("This is a crash");
+        getInstance().crash();
+    }
 
-        JSObject ret = new JSObject();
-        ret.put("value", value);
-        call.success(ret);
+    @PluginMethod()
+    public void logUser(PluginCall call) {
+        String id = call.getString("id");
+        String name = call.getString("name");
+        String email = call.getString("email");
+
+        if (id == null) {
+            call.reject("missing id property");
+        }
+
+        if (name == null) {
+            call.reject("missing name property");
+        }
+
+        if (email == null) {
+            call.reject("missing email property");
+        }
+
+        setUserIdentifier(id);
+        setUserEmail(email);
+        setUserName(name);
+
+        call.success();
+
     }
 }
